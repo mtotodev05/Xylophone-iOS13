f7b5a27daa03552c09d4438f2b2598161fc3eea3
mtotodev05 <mtotodev05@gmail.com>
2020-02-08 20:34:27 +0300

    Complete sound setting

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,16 +7,59 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var audioPlayer: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func onCKeyPressed(_ sender: UIButton) {
-        print("I was pressed")
+        self.onPlayKeySound("C",sender)
     }
     
+    @IBAction func onDKeyPressed(_ sender: UIButton) {
+        self.onPlayKeySound("D",sender)
+    }
+    
+    
+    @IBAction func onEKeyPressed(_ sender: UIButton) {
+        self.onPlayKeySound("E",sender)
+
+    }
+    
+    @IBAction func onFKeyPressed(_ sender: UIButton) {
+        self.onPlayKeySound("F",sender)
+
+    }
+    
+    
+    @IBAction func onGKeyPressed(_ sender: UIButton) {
+        self.onPlayKeySound("G",sender)
+    }
+    
+    
+    @IBAction func onAKeyPressed(_ sender: UIButton) {
+        self.onPlayKeySound("A",sender)
+
+    }
+
+
+    @IBAction func onBKeyPressed(_ sender: UIButton) {
+        self.onPlayKeySound("B",sender)
+
+    }
+    ;
+    private func onPlayKeySound(_ keyString: String = "C", _ actionSense: UIButton){
+        actionSense.alpha = 0.3
+        let keyUrl = Bundle.main.url(forResource: keyString, withExtension: "wav")
+        audioPlayer = try! AVAudioPlayer(contentsOf: keyUrl!)
+        audioPlayer.play()
+        
+        actionSense.alpha = 1
+    }
 }
 
